9b07c5f56910708fae4d13e351f3612ab9fef167
pbui <peter.j.bui@gmail.com>
2014-02-15 15:18:08 -0600

    Shift arguments so that the rest of parameters are the body

--- scripts/send.sh
+++ scripts/send.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+TYPE=$1
+SENDER=$2
+shift 2
+BODY=$@
+
 cat <<EOF | curl -X POST -d @- localhost:9411
 {
     "messages" : [
 	{
-	    "type"	: "$1",
-	    "sender"	: "$2",
-	    "body"	: "$3"
+	    "type"	: "${TYPE}",
+	    "sender"	: "${SENDER}",
+	    "body"	: "${BODY}"
 	}
     ]
 }
