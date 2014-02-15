4adba2287b6d7a0c367a52f54e8c2d56fd5ba59a
pbui <peter.j.bui@gmail.com>
2014-02-15 16:25:54 -0600

    Change path and enable debugging

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if ! ps ux | grep python2.7 | grep notifyd > /dev/null 2>&1; then
-    nice $HOME/bin/notifyd --port=9411 --log_file_prefix=$HOME/.config/notifyd/log &
+    nice $HOME/src/personal/notifyd/notifyd.py --port=9411 --logging=debug --log_file_prefix=$HOME/.config/notifyd/log &
 fi
