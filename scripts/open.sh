318597aceb2577a4818ece070137a13ec045c556
pbui <peter.j.bui@gmail.com>
2014-02-17 21:08:50 -0600

    Redirect stdout and stderr to /dev/null and exec

--- scripts/open.sh
+++ scripts/open.sh
@@ -9,8 +9,10 @@ BODY=$*
 
 case ${TYPE} in
     IMAGE|WWW)
-        xdg-open "${BODY}"
+        exec xdg-open "${BODY}" > /dev/null 2>&1
     ;;
 esac
+
+exit 0
 
 # vim: sts=4 sw=4 ts=8 expandtab ft=sh
