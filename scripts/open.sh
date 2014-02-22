a13394ebd2a3de0d0b7d89f6199843017074e02a
pbui <peter.j.bui@gmail.com>
2014-02-22 10:29:27 -0600

    Add open.sh script for opening files via the Notifyd StaticFileHandler

---
+++ scripts/open.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FILES_PATH=$HOME/.config/notifyd/files
+
+for file in "$@"; do
+    TYPE=WWW
+    SENDER=file
+    NAME=$(basename $file)
+    BODY=http://localhost:9411/files/$NAME
+
+    if ! cp "$file" $FILES_PATH/$NAME; then
+    	continue
+    fi
+
+    cat <<EOF | curl -X POST -d @- http://localhost:9411/messages
+    {
+	"messages" : [
+	    {
+		"type"	    : "${TYPE}",
+		"sender"    : "${SENDER}",
+		"body"	    : "${BODY}"
+	    }
+	]
+    }
+EOF
+done
