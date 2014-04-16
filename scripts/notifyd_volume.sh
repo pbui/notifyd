387f3bf492bfc7eabb30289b21504b878ee14445
pbui <peter.j.bui@gmail.com>
2014-04-15 20:55:03 -0500

    notifyd_volume: import linux version

--- scripts/notifyd_volume.sh
+++ scripts/notifyd_volume.sh
@@ -1,15 +1,43 @@
 #!/bin/sh
 
-current_volume() {
-    mixerctl outputs.master | cut -d = -f 2 | cut -d , -f 1
-}
+usage="usage: $0  [-c up|down|mute] [-i increment] [-m mixer]"
 
-current_mute() {
-    mixerctl outputs.master.mute | cut -d = -f 2
-}
+command=
+increment=
+mixer=
 
-if [ "$(current_mute)" = "on" ]; then
-    notifyd_send VOLUME MUTED
-else
-    notifyd_send VOLUME $((($(current_volume) + 1) * 100 / 255))%
+while getopts "c:i:m:h" o
+do case "$o" in
+    c) command=$OPTARG;;
+    i) increment=$OPTARG;;
+    m) mixer=$OPTARG;;
+    h) echo "$usage"; exit 0;;
+    ?) echo "$usage"; exit 0;;
+esac
+done
+
+if [[ -z $command ]] || [[ -z $increment ]] || [[ -z $mixer ]]
+then
+     echo $usage
+     exit 1
 fi
+
+display_volume=0
+if [ "$command" = "up" ]; then
+    display_volume=$(amixer set $mixer $increment+ unmute | grep -m 1 "%]" | cut -d "[" -f2|cut -d "%" -f1)%
+fi
+
+if [ "$command" = "down" ]; then
+    display_volume=$(amixer set $mixer $increment- unmute | grep -m 1 "%]" | cut -d "[" -f2|cut -d "%" -f1)%
+fi
+
+if [ "$command" = "mute" ]; then
+    if amixer get Master | grep "\[on\]" > /dev/null 2>&1; then
+        display_volume="Muted"
+        amixer set $mixer mute > /dev/null 2>&1
+    else
+        display_volume=$(amixer set $mixer unmute | grep -m 1 "%]" | cut -d "[" -f2|cut -d "%" -f1)%
+    fi
+fi
+
+notifyd_send "VOLUME" "${display_volume}"
