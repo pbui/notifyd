982da3aecb308ba7dba61fc3b80796d0edec563d
pbui <peter.j.bui@gmail.com>
2014-07-24 09:20:59 -0500

    Change debug to error message

--- notifyd.py
+++ notifyd.py
@@ -190,7 +190,7 @@ class NotifyDaemon(tornado.web.Application):
 
             self.add_messages(messages)
         except (TypeError, ValueError, KeyError) as e:
-            self.logger.debug('could not read json: {}\n{}'.format(data, e))
+            self.logger.error('could not read json: {}\n{}'.format(data, e))
 
         self.ioloop.add_timeout(datetime.timedelta(seconds=self.sleep), lambda: self.pull(peer))
 
