a8306cacf782ca9ece07ecccdade757598e6864e
pbui <peter.j.bui@gmail.com>
2014-08-24 01:13:24 -0500

    Backed out changeset 9183e0d18f63

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if ! pgrep -f notifyd > /dev/null 2>&1; then
+if ! ps ux | grep python2.7 | grep notifyd > /dev/null 2>&1; then
     nice $HOME/src/personal/notifyd/notifyd.py --port=9411 --log_file_prefix=$HOME/.config/notifyd/log &
 fi
