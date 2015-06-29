c56ac2d83ec1683c21ba23d4be0d7eb25cc0aa22
pbui <peter.j.bui@gmail.com>
2015-06-29 17:52:11 -0400

    notifyd_open: use google-drive-upload for certain files
    Fall back to opening in browser if it fails.

--- scripts/notifyd_open.sh
+++ scripts/notifyd_open.sh
@@ -5,12 +5,22 @@ FILES_PATH=$HOME/.config/notifyd/files
 for file in "$@"; do
     TYPE=WWW
     SENDER=file
-    NAME=$(basename $file)
-    BODY=http://localhost:9412/files/$NAME
+    NAME=$(basename "$file")
+    EXTENSION="${NAME##*.}"
 
-    if ! cp "$file" $FILES_PATH/$NAME; then
-    	continue
+    BODY="http://localhost:9412/files/$NAME"
+    if ! cp "$file" "$FILES_PATH/$NAME"; then
+	continue
     fi
+
+    case ${EXTENSION} in
+	doc|docx|xls|xlsx)
+	    BODY="$(google-drive-upload $file)"
+	    if [ -z ${BODY} ]; then
+		BODY="http://localhost:9412/files/$NAME"
+	    fi
+	;;
+    esac
 
     cat <<EOF | curl -X POST -d @- http://localhost:9411/messages
     {
