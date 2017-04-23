34fd3373574a1ffdf93e4d644ce26ca642c11ac8
pbui <peter.j.bui@gmail.com>
2017-04-22 23:27:38 -0400

    scripts/notifyd-toggle: send notification

--- scripts/notifyd-toggle.sh
+++ scripts/notifyd-toggle.sh
@@ -3,7 +3,9 @@
 DZEN2_SCRIPT=${HOME}/.config/notifyd/scripts/dzen2.sh
 
 if [ -x ${DZEN2_SCRIPT} ]; then
+    notifyd-send "NOTFIFYD" "Disable notifications" && sleep 2
     chmod -x ${DZEN2_SCRIPT}
 else
     chmod +x ${DZEN2_SCRIPT}
+    notifyd-send "NOTFIFYD" "Enable notifications"
 fi
