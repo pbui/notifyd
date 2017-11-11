bea346b3350fddbd42fd312757bffbb410ddcd18
Peter Bui <pbui@bx612.space>
2017-11-11 18:47:30 -0500

    notifyd-touchpad: use libinput

--- scripts/notifyd-touchpad.sh
+++ scripts/notifyd-touchpad.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-if [ $(synclient | grep TouchpadOff | awk '{print $3}') -eq 0 ]; then
-    synclient TouchpadOff=1
-    notifyd-send TOUCHPAD Off
-else
-    synclient TouchpadOff=0 VertEdgeScroll=1
-    notifyd-send TOUCHPAD On
-fi
+disable() {
+    xinput disable $ID
+    notifyd-send TOUCHPAD Disabled
+}
+
+enable() {
+    xinput enable $ID
+    xinput set-prop $ID "libinput Scroll Method Enabled" 0, 1, 0
+    notifyd-send TOUCHPAD Enabled
+}
+
+toggle() {
+    if [ $(xinput list-props $ID | grep 'Device Enabled' | awk '{print $4}') -eq 1 ]; then
+	disable
+    else
+	enable
+    fi
+}
+
+ID=$(xinput | grep TouchPad | cut -d = -f 2 | awk '{print $1}')
+
+case $1 in
+enable)	    enable;;
+disable)    disable;;
+*)	    toggle;;
+esac
