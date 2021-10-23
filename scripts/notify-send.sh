83b3ac3965dc683c3e2c690a2071d8b232c3f7e7
Peter Bui <pbui@deadpool.hsd1.in.comcast.net>
2021-10-23 15:10:19 -0400

    Add support for notify-send

---
+++ scripts/notify-send.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -lt 3 -o -z "$3" ]; then
+    icon=$1
+    title=$1 ; shift
+    body=$@
+else
+    icon=$1  ; shift
+    title=$1 ; shift
+    body=$@
+fi
+
+case $icon in
+    \#*)	icon=irc-chat;;
+    \&*)	icon=android-messages-desktop;;
+    CHAT)	icon=internet-chat;;
+    MAIL)	icon=internet-mail;;
+    PASSWORD)	icon=password-manager; title=Password;;
+    SCREENSHOT) icon=applets-screenshooter; title=Screenshot;;
+    PIANOBAR)	icon=google-music-manager-panel;;
+    TOUCHPAD)	icon=mouse; title=Touchpad;;
+    *)		icon=dialog-information;;
+esac
+
+notify-send --icon="$icon" "$title" "$body"
--- scripts/notify.sh
+++ scripts/notify.sh
@@ -4,6 +4,8 @@ PREFIX=$(dirname $0)
 
 if [ -x ${PREFIX}/dzen2.sh ]; then
     ${PREFIX}/dzen2.sh "$@"
+elif [ -x ${PREFIX}/notify-send.sh ]; then
+    ${PREFIX}/notify-send.sh "$@"
 fi
 
 if [ -x ${PREFIX}/dispatch.sh ]; then
